45dc0cad7c143690c9900bdc24a503f04972a190
Dinchev <ilasbg@abv.bg>
2017-05-30 19:05:07 +0300

    still testing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Carpets
 {
@@ -11,7 +7,7 @@ namespace Carpets
         static void Main(string[] args)
         {
             int x = int.Parse(Console.ReadLine());
-
+            Console.WriteLine("potato");
             int y = n / 2;
             string left = "/";
             string right = "\\";
